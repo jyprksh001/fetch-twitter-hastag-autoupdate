9a59b77f3b287d3b83443781e2ae4b807343a734
Jay Prakash <jay.jp.prakash@gmail.com>
2017-01-18 18:45:27 +0530

    changes

--- mongo.js
+++ mongo.js
@@ -1,5 +1,4 @@
 module.exports={
 //"database": 'mongodb://localhost:27017/tabletop',	
-'database':'mongodb://jyprksh001:pa55word@ds117919.mlab.com:17919/twitter-hashtag'
-
+"database":'mongodb://jyprksh001:pa55word@ds117919.mlab.com:17919/twitter-hashtag'
 }
