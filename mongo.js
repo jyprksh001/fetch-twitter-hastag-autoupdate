ce290a49c3eeb1b2c0de9212a23f77961ea15a76
Jay Prakash <jay.jp.prakash@gmail.com>
2017-01-18 18:32:54 +0530

    2nd commit

--- mongo.js
+++ mongo.js
@@ -1,3 +1,5 @@
 module.exports={
-"database": 'mongodb://localhost:27017/tabletop',	
+//"database": 'mongodb://localhost:27017/tabletop',	
+'database':'mongodb://jyprksh001:pa55word@ds117919.mlab.com:17919/twitter-hashtag'
+
 }
